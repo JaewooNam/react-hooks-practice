38cfd6eeb98c6284dc1d81d33367be7f18f00cee
JaewooNam <namssabu12@gmail.com>
2019-06-17 23:07:21 +0900

    Practice useInput

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 import React,{ useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue)
+  const onChange  = (event) => {
+    const {
+      target: { value }
+    } = event
+    let willUpdate = true
+    if (typeof validator === "function") {
+      willUpdate = validator(value)
+    }
+    if (willUpdate) {
+      setValue(value)
+    }
+  }
+
+  return { value, onChange }
+}
+
 const App = () => {
-  const [item, setItem] = useState(1)
-  const incrementItem = () => setItem(item + 1)
-  const decrementItem = () => setItem(item - 1)
+  const maxLength = (value) => value.length < 10
+  const name = useInput("Mr. ", maxLength)
   return (
     <div>
-      <h1>Hello {item}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <button onClick={incrementItem}>Increment</button>
-      <button onClick={decrementItem}>Decrement</button>
+      {console.log(name)}
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
     </div>
   )
 }
