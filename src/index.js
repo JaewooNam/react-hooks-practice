0aaec1ac966e51ed86948fc923e5d9c3839a807f
JaewooNam <namssabu12@gmail.com>
2019-07-15 11:28:34 +0900

    Practice useState

--- src/index.js
+++ src/index.js
@@ -1,37 +1,21 @@
 import React,{ useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const useInput = (initialValue, validator) => {
-  const [value, setValue] = useState(initialValue)
-  const onChange  = (event) => {
-    const {
-      target: { value }
-    } = event
-    let willUpdate = true
-    if (typeof validator === "function") {
-      willUpdate = validator(value)
-    }
-    if (willUpdate) {
-      setValue(value)
-    }
-  }
-
-  return { value, onChange }
-}
-
 const App = () => {
-  const maxLength = (value) => value.length < 10
-  const name = useInput("Mr. ", maxLength)
+  const [item, setItem] = useState(1)
+  const incrementItem = () => setItem(item + 1)
+  const decrementItem = () => setItem(item - 1)
   return (
-    <div>
-      {console.log(name)}
-      <h1>Hello</h1>
-      <input placeholder="Name" {...name} />
+    <div className="App">
+      <div>Hello {item}</div>
+      <button onClick={incrementItem}>Increment</button>
+      <button onClick={decrementItem}>Decrement</button>
     </div>
   )
 }
 
+const rootElement = document.getElementById("root")
 ReactDOM.render(
   <App />,
-  document.querySelector('#root')
+  rootElement
 )
